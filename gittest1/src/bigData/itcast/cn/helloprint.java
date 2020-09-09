6fdba376102ce5f5e1a696e9b1fcaab9b6d80cdd
qq452144 <452144@qq.com>
2020-09-09 18:04:30 +0800

    sdaf

---
+++ gittest1/src/bigData/itcast/cn/helloprint.java
@@ -0,0 +1,9 @@
+package bigData.itcast.cn;
+
+public class helloprint {
+
+    public static void main(String[] args) {
+        System.out.println("i am vipder , i love china ssss");
+        System.out.println("2222222222222");
+    }
+}
